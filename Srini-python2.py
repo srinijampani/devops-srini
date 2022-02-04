dcff0ab64b930798a029360d063d030989bdafa0
Srini <srinu.j9@gmail.com>
2022-02-04 11:04:33 +0530

    .\Srini-python2.py

--- Srini-python2.py
+++ Srini-python2.py
@@ -1,8 +1,6 @@
 hungry = input("are you hungry?")
 if hungry == "yes":
     print("eat some thing")
-    print("eat Pizza")
-    print("eat Junk food")
 else:
     print("Prepare DevOps")
     print("Prepare GitHub")
